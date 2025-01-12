711a9a0cfddbb8554df22a093917355aeac8e7b3
dinneth2007 <dinneceo@outlook.com>
2025-01-12 19:45:53 +0800

    Doctor managment CRUD interface done

--- src/main/java/org/example/controller/doctormanagerviewcontroller.java
+++ src/main/java/org/example/controller/doctormanagerviewcontroller.java
@@ -1,4 +1,61 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class doctormanagerviewcontroller {
+
+    @FXML
+    private Button BtnAdd;
+
+    @FXML
+    private Button BtnDlt;
+
+    @FXML
+    private Button BtnSearch;
+
+    @FXML
+    private Button BtnUpdate;
+
+    @FXML
+    private TextField txtAvailabilty;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextArea txtQuali;
+
+    @FXML
+    private TextField txtSpeciality;
+
+    @FXML
+    void BtnOnActionAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnOnActionDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnOnActionSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnOnActionUpdate(ActionEvent event) {
+
+    }
+
 }
